fae742830afcc23329fe43a4528ad02dd9b6ae70
Kyaw Kyaw Hein <hhein6223@gmail.com>
2024-01-26 00:13:39 +0630

    complete dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,14 +1,131 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageComponent from '../components/PageComponent'
 import axiosClient from '../axios-client'
-import { useStateContext } from '../context/StateContext'
+import DashboardCard from '../components/DashboardCard';
+import TButton from '../components/core/TButton';
+import { EyeIcon, PencilIcon } from '@heroicons/react/24/outline';
 
 const Dashboard = () => {
-    const {setUser,user} = useStateContext()
+    const [loading,setLoading] = useState(false);
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        setLoading(true);
+        axiosClient.get('/dashboard')
+        .then(({data})=>{
+            console.log(data);
+            setData(data)
+            setLoading(false)
+        })
+        .catch((err)=>{
+            setLoading(false)
+            console.log(err)
+        })
+    },[])
     return (
         <>
            <PageComponent title="Dashboard">
-                <h1>hello</h1>{user&&user.name}
+                {loading ? <div className="flex justify-center">Loading...</div> :
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 text-gray-700">
+                        <DashboardCard
+                            title="Total Surveys"
+                            className="order-1 lg:order-2"
+                            style={{ animationDelay: '0.1s' }}
+                        >
+                            <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
+                                {data.totalSurveys}
+                            </div>
+                        </DashboardCard>
+                        <DashboardCard
+                            title="Total Answers"
+                            className="order-2 lg:order-4"
+                            style={{ animationDelay: '0.2s' }}
+                        >
+                            <div className="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center">
+                                {data.totalAnswers}
+                            </div>
+                        </DashboardCard>
+                        <DashboardCard
+                            title="Latest Survey"
+                            className="order-3 lg:order-1 row-span-2"
+                            style={{ animationDelay: '0.2s' }}
+                        >
+                            {data.latestSurvey && (
+                                <div>
+                                    <img
+                                        src={data.latestSurvey.image_url}
+                                        className="w-[240px] mx-auto"
+                                    />
+                                    <h3 className="font-bold text-xl mb-3">
+                                        {data.latestSurvey.title}
+                                    </h3>
+                                    <div className="flex justify-between text-sm mb-1">
+                                        <div>Create Date:</div>
+                                        <div>{data.latestSurvey.created_at}</div>
+                                    </div>
+                                    <div className="flex justify-between text-sm mb-1">
+                                        <div>Expire Date:</div>
+                                        <div>{data.latestSurvey.expire_date}</div>
+                                    </div>
+                                    <div className="flex justify-between text-sm mb-1">
+                                        <div>Status:</div>
+                                        <div>{data.latestSurvey.status ? "Active" : "Draft"}</div>
+                                    </div>
+                                    <div className="flex justify-between text-sm mb-1">
+                                        <div>Questions:</div>
+                                        <div>{data.latestSurvey.questions}</div>
+                                    </div>
+                                    <div className="flex justify-between text-sm mb-3">
+                                        <div>Answers:</div>
+                                        <div>{data.latestSurvey.answers}</div>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <TButton to={`/surveys/${data.latestSurvey.id}`} link>
+                                            <PencilIcon className="w-5 h-5 mr-2" />
+                                            Edit Survey
+                                        </TButton>
+
+                                        <TButton link>
+                                            <EyeIcon className="w-5 h-5 mr-2" />
+                                            View Answers
+                                        </TButton>
+                                    </div>
+                                </div>
+                            )}
+                            {!data.latestSurvey && (
+                                <div className="text-gray-600 text-center py-16">
+                                    Your don't have surveys yet
+                                </div>
+                            )}
+                        </DashboardCard>
+                        <DashboardCard
+                            title="Latest Answers"
+                            className="order-4 lg:order-3 row-span-2"
+                            style={{ animationDelay: '0.3s' }}
+                        >
+                            {data.latestAnswers && data.latestAnswers.length!=0 ?
+                                <div className="text-left">
+                                    {data.latestAnswers.map((answer) => (
+                                        <a
+                                            href="#"
+                                            key={answer.id}
+                                            className="block p-2 hover:bg-gray-100/90"
+                                        >
+                                            <div className="font-semibold">{answer.title}</div>
+                                            <small>
+                                                Answer Made at:
+                                                <i className="font-semibold ml-2">{answer.end_date}</i>
+                                            </small>
+                                        </a>
+                                    ))}
+                                </div> :
+                                <div className="text-gray-600 text-center py-16">
+                                    Your don't have answers yet
+                                </div>
+                            }
+                        </DashboardCard>
+                    </div>
+            
+                }
            </PageComponent>
         </>
     )
